f6b704e4538ae3f58526bd879703c822a12e6cdb
Skyring-Marine <fernando.landeta@gmail.com>
2025-05-17 00:13:16 -0400

    test 2
    se agrando a pag web

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const hostname = '192.168.56.1'
-const port = 3000
+const hostname = '192.168.56.1';
+const port = 3000;
 
-const server = http.createServer( (req, res ) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text-plain')
-    res.end('Este es mi primer servidor en node!\n')
-})
+const server = http.createServer((req, res) => {
+    if (req.url === '/' || req.url === '/index.html') {
+        const filePath = path.join(__dirname, 'index.html');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Error interno del servidor');
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                res.end(data);
+            }
+        });
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Página no encontrada');
+    }
+});
 
-server.listen( port, hostname, () => {
-    console.log(`Server runnint at https://${hostname}:${port}/`)
-})
+server.listen(port, hostname, () => {
+    console.log(`Servidor corriendo en http://${hostname}:${port}/`);
+});
